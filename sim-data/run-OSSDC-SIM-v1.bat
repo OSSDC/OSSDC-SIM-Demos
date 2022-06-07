61475aefc2a442f01d745fdf3a8ab3c2fa781b4e
Andre Slavescu <51034490+AndreSlavescu@users.noreply.github.com>
2022-06-06 20:50:16 -0400

    Update to run from simulator folder

--- sim-data/run-OSSDC-SIM-v1.bat
+++ sim-data/run-OSSDC-SIM-v1.bat
@@ -1,1 +1,1 @@
-..\..\OSSDC-SIM-v1-Windows\OSSDC-SIM.exe --data .\2021.3-ossdc --cloudurl https://race.ossdc.org  --simid OSSDC-SIM-v1
+.\OSSDC-SIM.exe --data .\2021.3-ossdc --cloudurl https://race.ossdc.org  --simid OSSDC-SIM-v1
